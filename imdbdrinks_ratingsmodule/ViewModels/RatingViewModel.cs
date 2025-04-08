207f69c22da4906d760c368469524a5f0700b98f
Gatea David <davidgatea21@gmail.com>
2025-04-08 22:01:24 +0300

    Enhance RatingViewModel and ReviewViewModel with XML documentation for improved clarity. Updated property accessors and added nullability support for selected rating and review. Refactored loading methods to ensure proper data handling and notifications.

--- imdbdrinks_ratingsmodule/ViewModels/RatingViewModel.cs
+++ imdbdrinks_ratingsmodule/ViewModels/RatingViewModel.cs
@@ -8,11 +8,19 @@ using imdbdrinks_ratingsmodule.Services;
 
 namespace imdbdrinks_ratingsmodule.ViewModels
 {
+    /// <summary>
+    /// ViewModel for managing rating operations and data binding
+    /// </summary>
     public class RatingViewModel : INotifyPropertyChanged
     {
         private readonly RatingService _ratingService;
         private ObservableCollection<Rating> _ratings;
+        private Rating? _selectedRating;
+        private double _averageRating;
 
+        /// <summary>
+        /// Collection of ratings for the selected product
+        /// </summary>
         public ObservableCollection<Rating> Ratings
         {
             get => _ratings;
@@ -26,8 +34,10 @@ namespace imdbdrinks_ratingsmodule.ViewModels
             }
         }
 
-        private Rating _selectedRating;
-        public Rating SelectedRating
+        /// <summary>
+        /// Currently selected rating
+        /// </summary>
+        public Rating? SelectedRating
         {
             get => _selectedRating;
             set
@@ -40,11 +50,13 @@ namespace imdbdrinks_ratingsmodule.ViewModels
             }
         }
 
-        private double _averageRating;
+        /// <summary>
+        /// Average rating value for the selected product
+        /// </summary>
         public double AverageRating
         {
             get => _averageRating;
-            set
+            private set
             {
                 double roundedValue = Math.Round(value, 2);
                 if (_averageRating != roundedValue)
@@ -55,33 +67,56 @@ namespace imdbdrinks_ratingsmodule.ViewModels
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the RatingViewModel class
+        /// </summary>
+        /// <param name="ratingService">Service for rating data operations</param>
         public RatingViewModel(RatingService ratingService)
         {
             _ratingService = ratingService;
-            Ratings = new ObservableCollection<Rating>();
+            _ratings = new ObservableCollection<Rating>();
         }
 
+        /// <summary>
+        /// Loads ratings for the specified product
+        /// </summary>
+        /// <param name="productId">ID of the product to load ratings for</param>
         public void LoadRatingsForProduct(long productId)
         {
             var ratings = _ratingService.GetRatingsByProduct(productId);
             Ratings.Clear();
+            
             // Reverse the order so that the newest rating appears first
             foreach (var rating in ratings.Reverse())
             {
                 Ratings.Add(rating);
             }
+            
             AverageRating = _ratingService.GetAverageRating(productId);
         }
 
+        /// <summary>
+        /// Adds a new rating and refreshes the rating list
+        /// </summary>
+        /// <param name="rating">The rating to add</param>
         public void AddRating(Rating rating)
         {
             _ratingService.CreateRating(rating);
+            
             // Reload the ratings so that the new one appears at the top
             LoadRatingsForProduct(rating.ProductId);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        /// <summary>
+        /// Event for property change notifications
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Notifies subscribers that a property value has changed
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed</param>
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- imdbdrinks_ratingsmodule/ViewModels/ReviewViewModel.cs
+++ imdbdrinks_ratingsmodule/ViewModels/ReviewViewModel.cs
@@ -6,48 +6,92 @@ using imdbdrinks_ratingsmodule.Services;
 
 namespace imdbdrinks_ratingsmodule.ViewModels
 {
+    /// <summary>
+    /// ViewModel for managing review operations and data binding
+    /// </summary>
     public class ReviewViewModel : INotifyPropertyChanged
     {
         private readonly ReviewService _reviewService;
         private ObservableCollection<Review> _reviews;
+        private Review? _selectedReview;
 
+        /// <summary>
+        /// Collection of reviews for the selected rating
+        /// </summary>
         public ObservableCollection<Review> Reviews
         {
             get => _reviews;
-            set { _reviews = value; OnPropertyChanged(); }
+            set 
+            { 
+                if (_reviews != value)
+                {
+                    _reviews = value; 
+                    OnPropertyChanged();
+                }
+            }
         }
 
-        private Review _selectedReview;
-        public Review SelectedReview
+        /// <summary>
+        /// Currently selected review
+        /// </summary>
+        public Review? SelectedReview
         {
             get => _selectedReview;
-            set { _selectedReview = value; OnPropertyChanged(); }
+            set 
+            { 
+                if (_selectedReview != value)
+                {
+                    _selectedReview = value; 
+                    OnPropertyChanged();
+                }
+            }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the ReviewViewModel class
+        /// </summary>
+        /// <param name="reviewService">Service for review data operations</param>
         public ReviewViewModel(ReviewService reviewService)
         {
             _reviewService = reviewService;
-            Reviews = new ObservableCollection<Review>();
+            _reviews = new ObservableCollection<Review>();
         }
 
+        /// <summary>
+        /// Loads reviews associated with the specified rating
+        /// </summary>
+        /// <param name="ratingId">ID of the rating to load reviews for</param>
         public void LoadReviewsForRating(long ratingId)
         {
             var reviews = _reviewService.GetReviewsByRating(ratingId);
             Reviews.Clear();
+            
             foreach (var review in reviews)
             {
                 Reviews.Add(review);
             }
         }
 
+        /// <summary>
+        /// Adds a new review and refreshes the review list
+        /// </summary>
+        /// <param name="review">The review to add</param>
         public void AddReview(Review review)
         {
             _reviewService.CreateReview(review);
             LoadReviewsForRating(review.RatingId);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        /// <summary>
+        /// Event for property change notifications
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Notifies subscribers that a property value has changed
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed</param>
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
