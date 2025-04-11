4a6cb63f9dc22607b8c7633307d10bf62c14f4f9
bogdangosa <bogdan.gosa@gmail.com>
2025-04-11 19:46:59 +0300

    added basic structure for tests, created new project

---
+++ imdbdrinks_ratingsmodule.Test/DatabaseRatingRepository/DatabaseRatingRepositoryFindAllTest.cs
@@ -0,0 +1,17 @@
+﻿namespace imdbdrinks_ratingsmodule.Test;
+using FluentAssertions;
+using Moq;
+
+public class DatabaseRatingRepositoryFindAllTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
