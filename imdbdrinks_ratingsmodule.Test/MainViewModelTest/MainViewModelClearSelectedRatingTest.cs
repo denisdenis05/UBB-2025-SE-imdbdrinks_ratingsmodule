0b694b47132e018d815d943bf9883bb30467d385
Gatea David <davidgatea21@gmail.com>
2025-04-14 23:02:23 +0300

    Add internal testing support and refactor MainViewModel methods for better testability. Introduce new test helpers and unit tests for rating selection and initialization logic.

---
+++ imdbdrinks_ratingsmodule.Test/MainViewModelTest/MainViewModelClearSelectedRatingTest.cs
@@ -0,0 +1,91 @@
+using imdbdrinks_ratingsmodule.Domain;
+using imdbdrinks_ratingsmodule.Test.Helpers;
+using imdbdrinks_ratingsmodule.ViewModels;
+using Moq;
+
+namespace imdbdrinks_ratingsmodule.Test.MainViewModelTest
+{
+    public class MainViewModelClearSelectedRatingTest
+    {
+        private MainViewModel _viewModel;
+        private Mock<RatingViewModel> _mockRatingViewModel;
+        private Mock<ReviewViewModel> _mockReviewViewModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockRatingViewModel = MainViewModelTestHelper.CreateMockRatingViewModel();
+            _mockReviewViewModel = MainViewModelTestHelper.CreateMockReviewViewModel();
+            
+            _viewModel = MainViewModelTestHelper.CreateMainViewModel(
+                mockRatingViewModel: _mockRatingViewModel, 
+                mockReviewViewModel: _mockReviewViewModel);
+        }
+
+        [Test]
+        public void ClearSelectedRating_SetsSelectedRatingToNull()
+        {
+            // Arrange
+            var rating = new Rating { RatingId = 1, ProductId = 100, RatingValue = 4 };
+            _mockRatingViewModel.Object.SelectedRating = rating;
+
+            bool wasSetToNull = false;
+            _mockRatingViewModel.SetupSet(vm => vm.SelectedRating = null)
+                .Callback(() => wasSetToNull = true);
+
+            // Act
+            _viewModel.ClearSelectedRating();
+
+            // Assert
+            Assert.That(wasSetToNull, Is.True, "SelectedRating was not set to null");
+        }
+
+        [Test]
+        public void ClearSelectedRating_WithNullSelectedRating_StillClearsRating()
+        {
+            // Arrange - selected rating is already null
+            _mockRatingViewModel.Object.SelectedRating = null;
+
+            bool wasSetToNull = false;
+            _mockRatingViewModel.SetupSet(vm => vm.SelectedRating = null)
+                .Callback(() => wasSetToNull = true);
+
+            // Act
+            _viewModel.ClearSelectedRating();
+
+            // Assert - method should still be called
+            Assert.That(wasSetToNull, Is.True, "SelectedRating was not set to null");
+        }
+
+        [Test]
+        public void ClearSelectedRating_DoesNotAffectOtherProperties()
+        {
+            // Arrange
+            var rating = new Rating { RatingId = 1, ProductId = 100, RatingValue = 4 };
+            _mockRatingViewModel.Object.SelectedRating = rating;
+            
+            // Set up some other properties that shouldn't change
+            var ratings = new System.Collections.ObjectModel.ObservableCollection<Rating> { rating };
+            _mockRatingViewModel.Object.Ratings = ratings;
+            
+            // Store the initial state to compare after the method call
+            var initialRatings = _mockRatingViewModel.Object.Ratings;
+            
+            // Track if SelectedRating is set to null
+            bool selectedRatingWasSetToNull = false;
+            _mockRatingViewModel.SetupSet(vm => vm.SelectedRating = null)
+                .Callback(() => selectedRatingWasSetToNull = true);
+                
+            // Act
+            _viewModel.ClearSelectedRating();
+            
+            // Assert
+            Assert.That(selectedRatingWasSetToNull, Is.True, "SelectedRating was not set to null");
+            
+            // Verify Ratings collection is unchanged by comparing references
+            Assert.That(_mockRatingViewModel.Object.Ratings, Is.SameAs(initialRatings));
+            
+            // No need to verify ReviewViewModel as it's not touched by this method
+        }
+    }
+} 
---
+++ imdbdrinks_ratingsmodule.Test/MainViewModelTest/MainViewModelInitializationTest.cs
@@ -0,0 +1,146 @@
+using imdbdrinks_ratingsmodule.Domain;
+using imdbdrinks_ratingsmodule.Services;
+using imdbdrinks_ratingsmodule.Test.Helpers;
+using imdbdrinks_ratingsmodule.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace imdbdrinks_ratingsmodule.Test.MainViewModelTest
+{
+    public class MainViewModelInitializationTest
+    {
+        private Mock<IConfiguration> _mockConfiguration;
+        private Mock<RatingViewModel> _mockRatingViewModel;
+        private Mock<ReviewViewModel> _mockReviewViewModel;
+        private MainViewModel _viewModel;
+        private const int DefaultProductId = 100;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockConfiguration = MainViewModelTestHelper.CreateMockConfiguration();
+            _mockRatingViewModel = MainViewModelTestHelper.CreateMockRatingViewModel();
+            _mockReviewViewModel = MainViewModelTestHelper.CreateMockReviewViewModel();
+        }
+
+        [Test]
+        public void Constructor_InitializesProperties()
+        {
+            // Act
+            _viewModel = MainViewModelTestHelper.CreateMainViewModel(
+                _mockConfiguration,
+                _mockRatingViewModel,
+                _mockReviewViewModel);
+
+            // Assert
+            Assert.That(_viewModel.Configuration, Is.EqualTo(_mockConfiguration.Object));
+            Assert.That(_viewModel.RatingViewModel, Is.EqualTo(_mockRatingViewModel.Object));
+            Assert.That(_viewModel.ReviewViewModel, Is.EqualTo(_mockReviewViewModel.Object));
+        }
+
+        [Test]
+        public void Constructor_CallsLoadRatingsForProduct()
+        {
+            // Arrange - Implement a tracking flag in setup
+            bool loadRatingsWasCalled = false;
+            int productIdPassed = 0;
+            
+            _mockRatingViewModel.Setup(vm => vm.LoadRatingsForProduct(It.IsAny<int>()))
+                .Callback<int>(productId => {
+                    loadRatingsWasCalled = true;
+                    productIdPassed = productId;
+                });
+
+            // Act
+            _viewModel = MainViewModelTestHelper.CreateMainViewModel(
+                _mockConfiguration,
+                _mockRatingViewModel,
+                _mockReviewViewModel);
+
+            // Assert
+            Assert.That(loadRatingsWasCalled, Is.True, "LoadRatingsForProduct was not called");
+            Assert.That(productIdPassed, Is.EqualTo(DefaultProductId), "Incorrect product ID was passed");
+        }
+
+        [Test]
+        public void SelectedRating_ReturnsRatingFromRatingViewModel()
+        {
+            // Arrange
+            var expectedRating = new Rating { RatingId = 1, ProductId = 100, RatingValue = 4 };
+            _mockRatingViewModel.Setup(vm => vm.SelectedRating).Returns(expectedRating);
+            
+            _viewModel = MainViewModelTestHelper.CreateMainViewModel(
+                _mockConfiguration,
+                _mockRatingViewModel,
+                _mockReviewViewModel);
+
+            // Act
+            var result = _viewModel.SelectedRating;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedRating));
+        }
+
+        [Test]
+        public void RatingViewModel_SetProperty_NotifiesPropertyChanged()
+        {
+            // Arrange
+            _viewModel = MainViewModelTestHelper.CreateMainViewModel(
+                _mockConfiguration,
+                _mockRatingViewModel,
+                _mockReviewViewModel);
+
+            bool propertyChangedFired = false;
+            string propertyChangedName = null;
+
+            _viewModel.PropertyChanged += (sender, args) =>
+            {
+                propertyChangedFired = true;
+                propertyChangedName = args.PropertyName;
+            };
+
+            // Create a new mock to set
+            var newRatingViewModel = MainViewModelTestHelper.CreateMockRatingViewModel();
+
+            // Act
+            _viewModel.RatingViewModel = newRatingViewModel.Object;
+
+            // Assert
+            Assert.That(propertyChangedFired, Is.True);
+            Assert.That(propertyChangedName, Is.EqualTo(nameof(MainViewModel.RatingViewModel)));
+            Assert.That(_viewModel.RatingViewModel, Is.EqualTo(newRatingViewModel.Object));
+        }
+
+        [Test]
+        public void ReviewViewModel_SetProperty_NotifiesPropertyChanged()
+        {
+            // Arrange
+            _viewModel = MainViewModelTestHelper.CreateMainViewModel(
+                _mockConfiguration,
+                _mockRatingViewModel,
+                _mockReviewViewModel);
+
+            bool propertyChangedFired = false;
+            string propertyChangedName = null;
+
+            _viewModel.PropertyChanged += (sender, args) =>
+            {
+                propertyChangedFired = true;
+                propertyChangedName = args.PropertyName;
+            };
+
+            // Create a new mock to set
+            var newReviewViewModel = MainViewModelTestHelper.CreateMockReviewViewModel();
+
+            // Act
+            _viewModel.ReviewViewModel = newReviewViewModel.Object;
+
+            // Assert
+            Assert.That(propertyChangedFired, Is.True);
+            Assert.That(propertyChangedName, Is.EqualTo(nameof(MainViewModel.ReviewViewModel)));
+            Assert.That(_viewModel.ReviewViewModel, Is.EqualTo(newReviewViewModel.Object));
+        }
+    }
+} 
